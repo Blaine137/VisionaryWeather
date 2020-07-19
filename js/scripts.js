7e2769ec17ee3a5c6b7b96b25db3319d78ac7503
Blaine Young <blaine.young3553@gmail.com>
2020-07-19 10:26:45 -0400

    added logic for the active accordion

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,28 @@
-var data = null;
+window.addEventListener('load', () => {
 
-var xhr = new XMLHttpRequest();
+  var data = null;
 
-xhr.addEventListener("readystatechange", function () {
-  if (this.readyState === this.DONE) {
-    console.log(JSON.parse(this.responseText));
-  }
+  var xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === this.DONE) {
+      let data = Object.entries(JSON.parse(this.responseText)); //all of the data needed
+      console.log(data)
+        //gets the current temp
+      let tempData = Object.entries(data[2][1]);
+      let currentTemp = tempData[0][1] + " " + tempData[1][1];
+      document.querySelector('.mcdonoughTemp').innerHTML = "Current Temp: " + currentTemp;
+
+        //gets the current wind speed
+      let windSpeedData = Object.entries(data[3][1]);
+      let currentWindSpeed = windSpeedData[0][1] + " " + windSpeedData[1][1];
+      document.querySelector('.mcdonoughwindspeed').innerHTML = "Wind Speed: " + currentWindSpeed;
+      
+    }
+  });
+
+  xhr.open("GET", "https://api.climacell.co/v3/weather/realtime?lat=33.4473&lon=84.1469&unit_system=us&fields=temp,wind_speed,precipitation,precipitation_type&apikey=L3g9oaDS5EfduhTAcmPrVxbFSRjOJyrX");
+
+  xhr.send(data);
+
 });
-
-xhr.open("GET", "https://api.climacell.co/v3/weather/realtime?lat=33.4473&lon=84.1469&unit_system=us&fields=temp,precipitation,precipitation_type&apikey=L3g9oaDS5EfduhTAcmPrVxbFSRjOJyrX");
-
-xhr.send(data);
